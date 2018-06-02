c07b77ef36c25b1fbea278a43c5035de055f17c0
Courtney <clynnmorrison@gmail.com>
2018-06-02 11:36:56 -0600

    Commenting out view stuff

--- wedding/views.py
+++ wedding/views.py
@@ -19,7 +19,6 @@ def suggestions(request):
     the_page = response.read()
     return HttpResponse(the_page)
 
-
 @csrf_exempt
 def suggest(request):
     param_str = ""
@@ -34,11 +33,11 @@ def suggest(request):
     url = 'http://candwedding.weddingwoo.com/suggest'
     req = urllib2.Request(url, param_str)
 
-    req.add_header("Host", "candwedding.weddingwoo.com")
-    req.add_header("Origin", "http://candwedding.weddingwoo.com")
-    req.add_header("Referer", "http://candwedding.weddingwoo.com/song-requests")
+    #req.add_header("Host", "candwedding.weddingwoo.com")
+    #req.add_header("Origin", "http://candwedding.weddingwoo.com")
+    #req.add_header("Referer", "http://candwedding.weddingwoo.com/song-requests")
 
-    req.add_header("X-CSRF-Token", "iyGuiCqbEm9ACnZ4knYVFPtTEiOIACu5lQUfR2z7gnFxrmOdI1eQr8WE66kkr3w26iWPY8vXPqX/nzusqrD0Qg==")
+    #req.add_header("X-CSRF-Token", "iyGuiCqbEm9ACnZ4knYVFPtTEiOIACu5lQUfR2z7gnFxrmOdI1eQr8WE66kkr3w26iWPY8vXPqX/nzusqrD0Qg==")
     req.add_header("X-Requested-With", "XMLHttpRequest")
 
     response = urllib2.urlopen(req)
