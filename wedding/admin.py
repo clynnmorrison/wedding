d9370d0244f079252c9763b21e08143cd08d96de
Courtney <clynnmorrison@gmail.com>
2018-06-27 14:59:54 -0600

    Making radio fixes

--- wedding/admin.py
+++ wedding/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
-from .models import Rsvp
+from .models import Rsvp, UserProfile
 
+class UserProfileAdmin(admin.ModelAdmin):
+    model = UserProfile
 
 class RsvpAdmin(admin.ModelAdmin):
 	list_display = ("guest_name", "attending")
-
 	def guest_name(self, obj):
 		return obj.name if obj.name else "Guest"
 
 	guest_name.short_description = 'Name'
-
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Rsvp, RsvpAdmin)
