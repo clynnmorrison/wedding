41e3c1185de05d8c7d88d3a2cacd3f6425af9899
Courtney <clynnmorrison@gmail.com>
2018-06-30 12:22:17 -0600

    Adding stuff for save the date

--- wedding/management/commands/send_save_the_dates.py
+++ wedding/management/commands/send_save_the_dates.py
@@ -2,13 +2,16 @@ from django.core.management.base import BaseCommand
 from django.template import loader
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = 'Emails the save the date'
 
     def handle(self, *args, **options):
         template = loader.get_template("wedding/save_the_date.html")
-        html = template.render({"username": 'finke.dave@gmail.com', 'host_url': settings.HOST_URL})
+
+        user = User.objects.get(username="finke4@sbcglobal.net")
+        html = template.render({"username": user.username, 'addressee': user.first_name, 'host_url': settings.HOST_URL})
 
         email = EmailMultiAlternatives(
             subject="Dave & Courtney's wedding save the date",
